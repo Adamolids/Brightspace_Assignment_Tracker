914638ebf7f854fa03c01972bcbac53d4d99deba
Alexander Damolidis <damolida@sheridancollege.ca>
2026-03-24 23:45:38 -0400

    added logger and getRequired with null/empty check

--- src/main/java/com/alexdamolidis/util/Config.java
+++ src/main/java/com/alexdamolidis/util/Config.java
@@ -3,6 +3,8 @@ package com.alexdamolidis.util;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import com.alexdamolidis.exception.TrackerConfigException;
+
 public class Config {
     private static Properties properties = new Properties();
 
@@ -10,7 +12,7 @@ public class Config {
         try (FileInputStream fis = new FileInputStream(".env")) {            
             properties.load(fis);
         }catch(Exception e){
-            throw new RuntimeException("Could not load .env file. Please ensure it exists in the root directory.");
+            throw new TrackerConfigException("Could not load .env file. Please ensure it exists in the root directory.", e);
         }
     }
 
@@ -18,4 +20,12 @@ public class Config {
         return properties.getProperty(key);
     }
     
+    public static String getRequired(String key){
+        String value = get(key);
+        if(value == null || value.trim().isEmpty()){
+            throw new TrackerConfigException("Missing required config property: " + key +
+             "please check the readme and ensure your .env file is set up correctly.");
+        }
+        return value.trim();
+    }
 }
